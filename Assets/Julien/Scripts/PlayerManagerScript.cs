129ab490fba640b4f4256292fd0a0cfc6a8372c2
Antoine <a.leroux@rubika-edu.com>
2021-11-03 18:58:41 +0100

    Cursor player + balancing player

--- Assets/Julien/Scripts/PlayerManagerScript.cs
+++ Assets/Julien/Scripts/PlayerManagerScript.cs
@@ -7,10 +7,16 @@ public class PlayerManagerScript : MonoBehaviour
     public Transform spawner1, spawner2, spawner3, spawner4;
     private int nbPlayerActu = 0;
 
+    [Header("Players Cursor")]
+    public Sprite P1;
+    public Sprite P2;
+    public Sprite P3;
+    public Sprite P4;
+
     public void OnPlayerJoined(PlayerInput playerInput)
     {
         //initialisation des joueurs dans le menu selection
-        teleporterLobby.nbPlayerInGame++;
+        //teleporterLobby.nbPlayerInGame++;
 
         //instancialisation dans joueurs au d�but de chaque mapDebug.Log("player connected");
         switch (nbPlayerActu)
@@ -18,29 +24,33 @@ public class PlayerManagerScript : MonoBehaviour
             case 0:
                 playerInput.transform.position = new Vector2(spawner1.position.x, spawner1.position.y);
                 playerInput.GetComponent<PlayerController>().playerID = 0;
+                playerInput.transform.GetChild(6).GetComponent<SpriteRenderer>().sprite = P1;
                 break;
             case 1:
                 playerInput.transform.position = new Vector2(spawner2.position.x, spawner2.position.y);
                 playerInput.GetComponent<PlayerController>().playerID = 1;
+                playerInput.transform.GetChild(6).GetComponent<SpriteRenderer>().sprite = P2;
                 break;
             case 2:
                 playerInput.transform.position = new Vector2(spawner3.position.x, spawner3.position.y);
                 playerInput.GetComponent<PlayerController>().playerID = 2;
+                playerInput.transform.GetChild(6).GetComponent<SpriteRenderer>().sprite = P3;
                 break;
             case 3:
                 playerInput.transform.position = new Vector2(spawner4.position.x, spawner4.position.y);
                 playerInput.GetComponent<PlayerController>().playerID = 3;
+                playerInput.transform.GetChild(6).GetComponent<SpriteRenderer>().sprite = P4;
                 break;
         }
         nbPlayerActu++;
     }
 
-    public void OnPlayerLeft(PlayerInput playerInput)
+    /*public void OnPlayerLeft(PlayerInput playerInput)
     {
         //A v�rifier
         teleporterLobby.nbPlayerInGame++;
         nbPlayerActu--;
-    }
+    }*/
 
 
 }
