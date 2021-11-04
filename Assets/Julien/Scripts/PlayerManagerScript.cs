5287f22e7fe58e2872741ce145c0f54efab426f1
Antoine <a.leroux@rubika-edu.com>
2021-11-04 01:33:32 +0100

    Color switch on players

--- Assets/Julien/Scripts/PlayerManagerScript.cs
+++ Assets/Julien/Scripts/PlayerManagerScript.cs
@@ -8,6 +8,12 @@ public class PlayerManagerScript : MonoBehaviour
     private int nbPlayerActu = 0;
 
     [Header("Players Cursor")]
+    public Color colorP1;
+    public Color colorP2;
+    public Color colorP3;
+    public Color colorP4;
+
+    [Header("Players Cursor")]
     public Sprite P1;
     public Sprite P2;
     public Sprite P3;
@@ -25,24 +31,52 @@ public class PlayerManagerScript : MonoBehaviour
                 playerInput.transform.position = new Vector2(spawner1.position.x, spawner1.position.y);
                 playerInput.GetComponent<PlayerController>().playerID = 0;
                 playerInput.transform.GetChild(6).GetComponent<SpriteRenderer>().sprite = P1;
+
+                SpriteRenderer[] spritesP1 = spritesP1 = playerInput.GetComponentInChildren<PlayerAnim>().GetComponentsInChildren<SpriteRenderer>();
+                for (int i = 0; i < spritesP1.Length; i++)
+                {
+                    spritesP1[i].material.SetColor("_Color", colorP1);
+                }
+
                 DontDestroyOnLoad(playerInput);
                 break;
             case 1:
                 playerInput.transform.position = new Vector2(spawner2.position.x, spawner2.position.y);
                 playerInput.GetComponent<PlayerController>().playerID = 1;
                 playerInput.transform.GetChild(6).GetComponent<SpriteRenderer>().sprite = P2;
+
+                SpriteRenderer[] spritesP2 = spritesP2 = playerInput.GetComponentInChildren<PlayerAnim>().GetComponentsInChildren<SpriteRenderer>();
+                for (int i = 0; i < spritesP2.Length; i++)
+                {
+                    spritesP2[i].material.SetColor("_Color", colorP2);
+                }
+
                 DontDestroyOnLoad(playerInput);
                 break;
             case 2:
                 playerInput.transform.position = new Vector2(spawner3.position.x, spawner3.position.y);
                 playerInput.GetComponent<PlayerController>().playerID = 2;
                 playerInput.transform.GetChild(6).GetComponent<SpriteRenderer>().sprite = P3;
+
+                SpriteRenderer[] spritesP3 = spritesP3 = playerInput.GetComponentInChildren<PlayerAnim>().GetComponentsInChildren<SpriteRenderer>();
+                for (int i = 0; i < spritesP3.Length; i++)
+                {
+                    spritesP3[i].material.SetColor("_Color", colorP3);
+                }
+
                 DontDestroyOnLoad(playerInput);
                 break;
             case 3:
                 playerInput.transform.position = new Vector2(spawner4.position.x, spawner4.position.y);
                 playerInput.GetComponent<PlayerController>().playerID = 3;
                 playerInput.transform.GetChild(6).GetComponent<SpriteRenderer>().sprite = P4;
+
+                SpriteRenderer[] spritesP4 = spritesP4 = playerInput.GetComponentInChildren<PlayerAnim>().GetComponentsInChildren<SpriteRenderer>();
+                for (int i = 0; i < spritesP4.Length; i++)
+                {
+                    spritesP4[i].material.SetColor("_Color", colorP4);
+                }
+
                 DontDestroyOnLoad(playerInput);
                 break;
         }
