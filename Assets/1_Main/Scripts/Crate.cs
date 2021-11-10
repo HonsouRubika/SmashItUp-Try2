7ba71a5ecb2d3c2b5145006456ec2f348f72fa0e
Antoine <a.leroux@rubika-edu.com>
2021-11-10 11:20:18 +0100

    Crate floating point feedback on which player destroy it

--- Assets/1_Main/Scripts/Crate.cs
+++ Assets/1_Main/Scripts/Crate.cs
@@ -15,6 +15,10 @@ public class Crate : MonoBehaviour
     [Header("Score")]
     public int pointsToAdd = 1;
     public GameObject floatingPoint;
+    public Color player0Color;
+    public Color player1Color;
+    public Color player2Color;
+    public Color player3Color;
 
     private Collider2D hammerCollider = null;
     private BoxCollider2D crateCollider;
@@ -48,29 +52,39 @@ public class Crate : MonoBehaviour
     private void Update()
     {
         if (hammerCollider != null)
-        {
+        {          
+            GameObject floatPoint = Instantiate(floatingPoint, transform.position, Quaternion.identity);
+            floatPoint.transform.GetChild(0).GetComponent<TextMeshPro>().text = "+ " + pointsToAdd.ToString();
+
             switch (hammerCollider.GetComponentInParent<PlayerController>().playerID)
             {
                 case 0:
+                    floatPoint.transform.GetChild(0).GetComponent<TextMeshPro>().color = player0Color;
                     scoreScript.AddScore(pointsToAdd, 0, 0, 0);
                     break;
                 case 1:
+                    floatPoint.transform.GetChild(0).GetComponent<TextMeshPro>().color = player1Color;
                     scoreScript.AddScore(0, pointsToAdd, 0, 0);
                     break;
                 case 2:
+                    floatPoint.transform.GetChild(0).GetComponent<TextMeshPro>().color = player2Color;
                     scoreScript.AddScore(0, 0, pointsToAdd, 0);
                     break;
                 case 3:
+                    floatPoint.transform.GetChild(0).GetComponent<TextMeshPro>().color = player3Color;
                     scoreScript.AddScore(0, 0, 0, pointsToAdd);
                     break;
             }
 
-            GameObject floatPoint = Instantiate(floatingPoint, transform.position, Quaternion.identity);
-            floatPoint.transform.GetChild(0).GetComponent<TextMeshPro>().text = "+ " + pointsToAdd.ToString();
-            detructCrateScript.cratesNumber--;
-            Destroy(gameObject);
-            destructSoundScript.PlayerDestroy();
+            DestroyCrate();
         }
+    }
+
+    private void DestroyCrate()
+    {
+        detructCrateScript.cratesNumber--;
+        Destroy(gameObject);
+        destructSoundScript.PlayerDestroy();
     }
 
     /* Renderer the overlappBox 
