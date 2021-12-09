2b4156f765ed4c753a4452ddbcb8209c457343fc
Julien Rubika <j.haigron@rubika-edu.com>
2021-12-09 01:16:00 +0100

    TeamCompo
    shuffle playersCompo

--- Assets/1_Main/Scripts/Managers/GameManager.cs
+++ Assets/1_Main/Scripts/Managers/GameManager.cs
@@ -164,7 +164,7 @@ public class GameManager : MonoBehaviour
             }
             _selectedGameModes[i] = nextGameMode;
             _teamCompo[i] = Random.Range(0, (int)TeamCompo.Coop); //on retire la coop des Compo d'equipe possible
-            Debug.Log("Team compo : " +_teamCompo[i]);
+            //Debug.Log("Team compo : " +_teamCompo[i]);
             //Debug.Log(i + " : " +_selectedGameModes[i]);
         }
 
