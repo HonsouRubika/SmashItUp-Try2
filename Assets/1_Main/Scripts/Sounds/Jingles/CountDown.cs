a557b15e9ccb72616bfa8ca655d3d4266a52a415
Antoine <a.leroux@rubika-edu.com>
2021-12-13 12:28:48 +0100

    reintregrate textMeshPro

--- Assets/1_Main/Scripts/Sounds/Jingles/CountDown.cs
+++ Assets/1_Main/Scripts/Sounds/Jingles/CountDown.cs
@@ -2,12 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CountDownSFX : MonoBehaviour
+public class CountDown : MonoBehaviour
 {
     [Space]
     [Header("CountDown Sound")]
     public AudioClip countDown;
     [Range(0f, 1f)] public float countDownVolume = 0.5f;
+
+    public Timer timerScript;
+    private bool playOneTime = false;
+
+    private void Update()
+    {
+        if (timerScript.miniGameTimer <= 5 && !playOneTime)
+        {
+            PlayerInLastSeconds();
+            playOneTime = true;
+        }
+    }
 
     public void PlayerInLastSeconds()
     {
