83857eb78b01b5e7af2730f035c0778ac75c3996
Antoine <a.leroux@rubika-edu.com>
2022-01-15 11:54:22 +0100

    Music loop on all scenes

--- Assets/1_Main/Scripts/Sounds/Music_Mini_Game.cs
+++ Assets/1_Main/Scripts/Sounds/Music_Mini_Game.cs
@@ -9,9 +9,18 @@ public class Music_Mini_Game : MonoBehaviour
     [Header("Music Mini Game")]
     public AudioClip musicMiniGame;
     [Range(0f, 1f)] public float musicMiniGameVolume = 0.5f;
+    public bool loopMusic = true;
 
     private void Start()
     {
         SoundManager.Instance.PlayMusic(musicMiniGame, musicMiniGameVolume);
+    }
+
+    private void Update()
+    {
+        if (loopMusic && !SoundManager.Instance.musicSource.isPlaying)
+        {
+            SoundManager.Instance.PlayMusic(musicMiniGame, musicMiniGameVolume);
+        }
     }
 }
