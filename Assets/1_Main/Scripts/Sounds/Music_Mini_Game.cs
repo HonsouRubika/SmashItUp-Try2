bcdac74a88b6a63757ea79204d6a0845df1db2d5
Antoine <a.leroux@rubika-edu.com>
2022-01-15 12:05:08 +0100

    base created for play 30,45 & 60s music in mini-game

--- Assets/1_Main/Scripts/Sounds/Music_Mini_Game.cs
+++ Assets/1_Main/Scripts/Sounds/Music_Mini_Game.cs
@@ -8,19 +8,46 @@ public class Music_Mini_Game : MonoBehaviour
     [Space]
     [Header("Music Mini Game")]
     public AudioClip musicMiniGame;
+
+    [Header("Timed music Mini game")]
+    public AudioClip musicMiniGame30s;
+    public AudioClip musicMiniGame45s;
+    public AudioClip musicMiniGame60s;
+
+    private AudioClip currentMusicMiniGame;
     [Range(0f, 1f)] public float musicMiniGameVolume = 0.5f;
     public bool loopMusic = true;
 
     private void Start()
     {
-        SoundManager.Instance.PlayMusic(musicMiniGame, musicMiniGameVolume);
+        if (musicMiniGame30s == null || musicMiniGame45s == null || musicMiniGame60s == null)
+        {
+            currentMusicMiniGame = musicMiniGame;
+        }
+        else
+        {
+            switch (GameManager.Instance.durationMiniGame)
+            {
+                case 30:
+                    currentMusicMiniGame = musicMiniGame30s;
+                    break;
+                case 45:
+                    currentMusicMiniGame = musicMiniGame45s;
+                    break;
+                case 60:
+                    currentMusicMiniGame = musicMiniGame60s;
+                    break;
+            }
+        }
+
+        SoundManager.Instance.PlayMusic(currentMusicMiniGame, musicMiniGameVolume);
     }
 
     private void Update()
     {
         if (loopMusic && !SoundManager.Instance.musicSource.isPlaying)
         {
-            SoundManager.Instance.PlayMusic(musicMiniGame, musicMiniGameVolume);
+            SoundManager.Instance.PlayMusic(currentMusicMiniGame, musicMiniGameVolume);
         }
     }
 }
