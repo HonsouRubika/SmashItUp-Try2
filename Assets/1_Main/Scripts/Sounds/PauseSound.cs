de4a637ae91c5a370f1f3afd505146791f92ccef
Gauthier <g.gobert@rubika-edu.com>
2022-01-21 18:27:45 +0100

    Music Mole

---
+++ Assets/1_Main/Scripts/Sounds/PauseSound.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseSound : MonoBehaviour
+{
+    [Space]
+    [Header("Pause Sound")]
+    public AudioClip pause;
+    [Range(0f, 1f)] public float pauseVolume = 0.5f;
+
+    public void GameIsOnPause()
+    {
+        SoundManager.Instance.PlaySfx(pause, pauseVolume);
+    }
+}
