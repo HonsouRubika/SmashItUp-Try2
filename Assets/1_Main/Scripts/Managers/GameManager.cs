7682b54bd7e85f7e93625ed9944dcfef5c3547dc
Julien Rubika <j.haigron@rubika-edu.com>
2022-01-24 00:42:56 +0100

    oui

--- Assets/1_Main/Scripts/Managers/GameManager.cs
+++ Assets/1_Main/Scripts/Managers/GameManager.cs
@@ -91,8 +91,6 @@ public class GameManager : MonoBehaviour
         {
             ProgressionData pd = SaveSystem.LoadProgression();
             nbGameFinished = pd.GetNbGameFinished();
-            //Debug.Log(nbGameFinished);
-            nbGameFinished = 0;
         }
     }
 
